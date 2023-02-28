9e011179047a246567c0a6e069178f8deba519f4
Hüseyin Çınar <84516708+Huseyin-Cinar@users.noreply.github.com>
2023-02-28 13:36:21 +0300

    Create user_system.js

---
+++ user_system.js
@@ -0,0 +1,16 @@
+function fetchData() {
+
+  var data = fetch("./user_system.json")
+         .then(response=>response.json())
+         .then(veri=> {
+           document.getElementById("username").innerHTML = veri["user_0"].username
+           document.getElementById("avatar").innerHTML = veri["user_0"].avatar
+           document.getElementById("email").innerHTML = veri["user_0"].email
+           document.getElementById("theme").innerHTML = veri["user_0"].theme
+           document.getElementById("password").innerHTML = veri["user_0"].password
+           document.getElementById("birth_date").innerHTML = veri["user_0"].birth_date
+         }),
+
+};
+
+fetchData()
