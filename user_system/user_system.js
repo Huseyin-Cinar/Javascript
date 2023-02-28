057165cad50f0ea5f8ab3e41707de695e8101c4b
Hüseyin Çınar <84516708+Huseyin-Cinar@users.noreply.github.com>
2023-02-28 13:43:09 +0300

    Update user_system.js

--- user_system/user_system.js
+++ user_system/user_system.js
@@ -3,13 +3,13 @@ function fetchData() {
   var data = fetch("./user_system.json")
          .then(response=>response.json())
          .then(veri=> {
-           document.getElementById("username").innerHTML = veri["user_0"].username
-           document.getElementById("avatar").innerHTML = veri["user_0"].avatar
-           document.getElementById("email").innerHTML = veri["user_0"].email
-           document.getElementById("theme").innerHTML = veri["user_0"].theme
-           document.getElementById("password").innerHTML = veri["user_0"].password
-           document.getElementById("birth_date").innerHTML = veri["user_0"].birth_date
-           document.getElementById("premium").innerHTML = veri["user_0"].premium
+           document.getElementById("username").innerHTML = veri["User_0"].username
+           document.getElementById("avatar").innerHTML = veri["User_0"].avatar
+           document.getElementById("email").innerHTML = veri["User_0"].email
+           document.getElementById("theme").innerHTML = veri["User_0"].theme
+           document.getElementById("password").innerHTML = veri["User_0"].password
+           document.getElementById("birth_date").innerHTML = veri["User_0"].birth_date
+           document.getElementById("premium").innerHTML = veri["User_0"].premium
          }),
 
 };
